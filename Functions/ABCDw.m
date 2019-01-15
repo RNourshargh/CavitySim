005d024f98b95c081c4e840272a4979a1ac5c401
RNourshargh <44952231+RNourshargh@users.noreply.github.com>
2019-01-15 19:15:51 +0000

    Added comment

--- Functions/ABCDw.m
+++ Functions/ABCDw.m
@@ -2,7 +2,8 @@ function [ w ] = ABCDw( ABCD )
 %ANCDw This calculates the spot size in a resonator in the plane at which
 %the ABCD matrix is defined
 
-[~,Lambda,~]=ConfigCavEnv();
+%Recalling constant values, wavelength
+[~,Lambda]=ConfigCavEnv();
 
 A = ABCD(1,1);
 B = ABCD(1,2);
