c848b651feb0ef1458f4ce85f3d2bc3522d9e2b1
RNourshargh <44952231+RNourshargh@users.noreply.github.com>
2019-01-15 15:04:02 +0000

    Reorder ConfigCavEnv outputs to put array first and make more robust

--- Functions/ConfigCavEnv.m
+++ Functions/ConfigCavEnv.m
@@ -1,9 +1,9 @@
-function [M] = ConfigCavEnv()
-%ConfigCav Imports the ConfigCavENv CSV and outputs an array containing
+function [M,Lambda, Test] = ConfigCavEnv()
+%ConfigCav Imports the ConfigCavEnv CSV and outputs an array containing
 %these evironmental variables, and the individual variables.
 M = csvread('ConfigCavEnv.csv',1,0);
 
-%Lambda = M(1,1)
-%Test = M(1,2)
+Lambda = M(1,1);
+Test = M(1,2);
 end
 
