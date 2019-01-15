07ff041016253f68fbf1753807e6ed506afa5c8e
RNourshargh <44952231+RNourshargh@users.noreply.github.com>
2019-01-15 15:27:13 +0000

    Modified functions to take Lambda from the ConfigCavEnv.csv file, Updated comments

--- Functions/RadFromq.m
+++ Functions/RadFromq.m
@@ -2,10 +2,8 @@ function [ w ] = RadFromq( q )
 %RadFromq calculates the beam radius from the complex radius of curvature
 %at the point where the radius of curvature is defined
 
-Lambda = 780E-9;
+[~,Lambda,~]=ConfigCavEnv();
 
 w = sqrt(-Lambda/(pi* Imag(1/q)));
-
-
 end
 
--- Functions/RayleighRangeW0.m
+++ Functions/RayleighRangeW0.m
@@ -1,16 +1,10 @@
-function [ zr ] = RayleighRangeW0( w0, lambda )
-%This calculates the Rayleigh range from a beam waist and wavelength.
-%If no wavelength is given it is asumed to be 780nm
+function [ zr ] = RayleighRangeW0(w0)
+%This calculates the Rayleigh range from the beam waist using the
+%ConfigCavEnv value for wavelength
 %All units are in metres
-switch nargin
-        case 2
-            L = lambda;
-        case 1
-            L=780E-9;
-        otherwise
-            disp('Error incorrect number of variables')
-end
-zr = pi* w0^2/L; 
 
+[~,Lambda,~]=ConfigCavEnv();
+
+zr = pi* w0^2/Lambda;
 end
 
