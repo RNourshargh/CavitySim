a1d7ad63b50efba2dab2f34100538a436f80619a
RNourshargh <44952231+RNourshargh@users.noreply.github.com>
2019-01-17 21:53:01 +0000

    Complete and push CavityDesign1.dat, add all files and dirs to repo, update CavityPath.txt

---
+++ Functions/ReadCavityDesign.m
@@ -0,0 +1,8 @@
+function [CavityDesign] = ReadCavityDesign(CavityDesignFile)
+%% ReadCavityFile imports the CavityDesign.dat file to a string array output
+
+CavityDesign = ImportLineSepSTR(CavityDesignFile)
+
+
+end
+
