aa567f6c559a023ac5648ffeb82c8f74c4c685c0
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-01-23 14:55:18 -0800

    Pass tests locally

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 
-from cavitysim.utils import pathfreespace
+from cavitysim.utils import path_free_space
 
 
 def test_pathfreespace():
-    test_pathlength = 7.0
+    test_path_length = 7.0
     test_n0 = 1.1
 
-    expected = (np.array([[1, test_pathlength], [0, 1]]), test_pathlength*test_n0)
-    result = pathfreespace(test_pathlength, test_n0)
+    expected_abcd = np.array([[1, test_path_length], [0, 1]])
+    expected_opl =  test_path_length*test_n0
+    result_abcd, result_opl = path_free_space(test_path_length, test_n0)
 
-    assert expected == result
+    np.testing.assert_allclose(expected_abcd, result_abcd)
+    assert expected_opl == result_opl
