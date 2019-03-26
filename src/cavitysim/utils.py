a7a4af04a901fc02ac2317e55984e1ef86739c43
RNourshargh <44952231+RNourshargh@users.noreply.github.com>
2019-03-26 21:17:05 +0000

    improve abcd_stability tests remove unneccessary elseif, else

--- src/cavitysim/utils.py
+++ src/cavitysim/utils.py
@@ -65,13 +65,13 @@ def abcd_stability(abcd):
         print("Cavity is stable")
         print("A+D = " +str(AD))
         CavStab = True
-    elif (AD>2) or (AD<-2):
+    else (AD>2) or (AD<-2):
         print("Cavity is unstable")
         print("A+D = " +str(AD))
         CavStab = False
-    else:
-        print("Error:Invalid abcd matrix")
-        CavStab = False
+#    else:
+#        print("Error:Invalid abcd matrix")
+#        CavStab = False
     return AD, CavStab
 
 
