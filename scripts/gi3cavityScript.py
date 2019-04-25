0b9fd85973e17cccc3cac0e574bd8d7884522147
RNourshargh <44952231+RNourshargh@users.noreply.github.com>
2019-04-25 12:01:15 +0100

    scratching

--- scripts/gi3cavityScript.py
+++ scripts/gi3cavityScript.py
@@ -1,48 +1,27 @@
 from cavitysim.utils import cavity, mirror_normal, path_constant_index, lens_thin_vac
 
-#cavity_elements_1 = [
-#        mirror_normal(),
-#        path_constant_index(),
-#        lens_thin_vac(),
-#        path_constant_index(),
-#        lens_thin_vac(0.1), #Thorlabs lens
-#        path_constant_index(0.4),
-#        mirror_normal()
-#        ]
 
-#"""THe following setup produces a 6mm beam diameter"""
-#cavity_elements_2 = [
-#        mirror_normal(),
-#        path_constant_index(0.4),
-#        lens_thin_vac(0.1),
-#        path_constant_index(0.1077),
-#        lens_thin_vac(0.0075), #Thorlabs lens
-#        path_constant_index(0.11),
-#        mirror_normal()
-#]
 #"""THe following setup produces a 6mm beam diameter and is hopefully less scary"""
 #cavity_elements_2 = [
 #        mirror_normal(),
 #        path_constant_index(0.4),
-#        lens_thin_vac(0.2),
-#        path_constant_index(0.2165),
-#        lens_thin_vac(0.015), #Thorlabs lens
+#        lens_thin_vac(0.15), #Edmund, query 785nm coated lenses
+#        path_constant_index(0.1625),
+#        lens_thin_vac(0.012), #Thorlabs lens
 #        path_constant_index(0.11),
 #        mirror_normal()
 #]
 
-
-"""THe following setup produces a 6mm beam diameter and is hopefully less scary"""
+"""THe following setup is reversed to see the small mode produces a 6mm beam diameter and is hopefully less scary"""
 cavity_elements_2 = [
         mirror_normal(),
-        path_constant_index(0.4),
-        lens_thin_vac(0.15), #Edmund, query 785nm coated lenses
-        path_constant_index(0.1628),
-        lens_thin_vac(0.0125), #Thorlabs lens
         path_constant_index(0.11),
+        lens_thin_vac(0.012), #Thorlabs lens
+        path_constant_index(0.1625),
+        lens_thin_vac(0.15), #thorlabs V coated
+        path_constant_index(0.4),
         mirror_normal()
 ]
-
 
 
 
