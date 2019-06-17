b3defd4905564efbc1ec89c8893b75e784fdb117
RNourshargh <44952231+RNourshargh@users.noreply.github.com>
2019-06-17 20:13:25 +0100

    Added some hacking around scrips to gi3cavity

--- scripts/gi3cavityScript.py
+++ scripts/gi3cavityScript.py
@@ -2,29 +2,41 @@ from cavitysim.utils import Cavity, MirrorNormal, PathConstantIndex, LensThinVac
 import numpy as np
 
 
-#"""THe following setup produces a 6mm beam diameter and is hopefully less scary"""
-#cavity_elements_2 = [
-#        mirror_normal(),
-#        path_constant_index(0.4),
-#        lens_thin_vac(0.15), #Edmund, query 785nm coated lenses
-#        path_constant_index(0.1625),
-#        lens_thin_vac(0.012), #Thorlabs lens
-#        path_constant_index(0.11),
-#        mirror_normal()
-#]
 
-"""THe following setup is reversed to see the small mode produces a 6mm beam diameter and is hopefully less scary"""
+
 cavity_elements = [
         MirrorNormal(),
         PathConstantIndex(0.4),
         LensThinVac(0.15), #Edmund, query 785nm coated lenses
         PathConstantIndex(0.1625),# Standard length 0.1625
         LensThinVac(0.012), #Thorlabs lens
-        PathConstantIndex(0.11), #Standard length 0.11
+        PathConstantIndex(0.12), #Standard length 0.11
         MirrorNormal()
 ]
 
+reversed_cavity_elements = [
+        MirrorNormal(),
+        PathConstantIndex(0.12), #Standard length 0.11
+        LensThinVac(0.012), #Thorlabs lens
+        PathConstantIndex(0.1625),# Standard length 0.1625
+        LensThinVac(0.15), #Edmund, query 785nm coated lenses
+        PathConstantIndex(0.4),
+        MirrorNormal()
+]
+
+
 GI3Cavity = Cavity(cavity_elements, True)
+GI3CavityReversed = Cavity(reversed_cavity_elements, True)
+
+CavityInputRadius = GI3CavityReversed.end_radius()
+
+print("Cavity Input radius is:", CavityInputRadius)
+
+FibreOutRadius = 0.00043
+
+LensFocalLength = (np.pi*CavityInputRadius**2)/(780e-9) * np.sqrt((FibreOutRadius/CavityInputRadius)**2-1)
+print("Lens Focal Length:", LensFocalLength)
+
 
 m = GI3Cavity.ad()/2
 print("Half Trace is:", m)
